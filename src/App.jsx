75ac8f2a298e967fbe0961ed8ea0820c2e5b4d00
Md Shafayatul Haque <shafayat@Mds-MacBook-Air-2.local>
2025-04-17 19:06:16 +0600

    added navbar

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import "./App.css";
+import { LoadingScreen } from "./components/LoadingScreen";
+import "./index.css";
+import { Navbar } from "./components/Navbar";
+import { MobileMenu } from "./components/MobileMenu";
+function App() {
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  return (
+    <>
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${
+          isLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-grey-100`}
+      ></div>
+      <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen}></Navbar>
+      <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen}></MobileMenu>
+    </>
+  );
+}
+
+export default App;
